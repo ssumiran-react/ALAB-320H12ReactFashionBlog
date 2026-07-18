10a19f52cb5ed9deb56fdec75c7001e1acdde462
ssumiran <ssumiran@yahoo.com>
2026-07-18 17:52:02 -0400

    CSS navbar to row lining

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,11 +1,12 @@
 
 export default function Article(props){
-
+  const imagePath = `${props.artImgUrl}`;
+  console.log("te ",imagePath)
   return (
     <>
       <h2 style={{color: "gray"}}>{props.artDate}</h2>
       <h2 className="articleTitle">{props.artTitle}</h2>
-      <img src={props.artImgUrl} alt={props.artTitle}/>
+      <img style={{width:"100%"}} src={imagePath} alt={props.artTitle}/>
       <p>{props.artContent}</p>
       <div style={{color: "red",textAlign: "right"}}>
         <a ><strong>Continues ...</strong></a>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,18 +6,17 @@ export default function Main() {
     {
       artDate: "11/22/26",
       artTitle: "On the Street in Brooklyn",
-      artImgUrl: "../assets/fashion/blobimg1.jpg",
+      artImgUrl: "/fashion/blobimg1.png",
       artContent: "OLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum"
     },
     {
       artDate: "11/21/26",
       artTitle: "Vintage in Vogue",
-      artImgUrl: "../assets/fashion/blobimg2.jpg",
+      artImgUrl: "/fashion/blobimg2.jpg",
       artContent: "simply dummy text of the Lorem Ipsum is  printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum"
     }
   ]
   // <img src="../assets/fashion/blobimg1.jpg"
-
   return (
     <article>
       {articleList.map((a) =>
